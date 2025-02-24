e4716fcdd57b10d1a97f7a4be226fc95b2209293
Wildwurger <taekyung1492@gmail.com>
2025-02-24 17:31:46 +0900

    2025-02-24 헤더 네비바 기본 요소 생성

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-
+import Header from "./component/Header";
 import ProfilePage from "./page/profile/ProfilePage";
 
 function App() {
   return (
     <div>
       <Router>
+        <Header />
         <main style={{ maxWidth: "1200px", margin: "0 auto", padding: "16px" }}>
           <Routes>
             <Route path="/" element={<ProfilePage />} />
