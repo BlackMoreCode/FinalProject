f755bf13db1e6bb7e8cf1efbded0c7e07f5c82ff
kimju9908 <kimj9908@naver.com>
2025-02-28 17:06:02 +0900

    음식레시피 데이터

---
+++ back/src/main/java/com/kh/back/dto/recipe/res/FoodResDto.java
@@ -0,0 +1,41 @@
+package com.kh.back.dto.recipe.res;
+
+import com.kh.back.dto.python.SearchResDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FoodResDto  implements SearchResDto {
+    private String name;
+    private String RCP_WAY2;
+    private String RCP_PAT2;
+    private String RCP_NA_TIP;
+    private List<IngredientDto> ingredients;
+    private List<ManualDto> manuals;
+    private String ATT_FILE_NO_MAIN;
+    private String ATT_FILE_NO_MK;
+    private int like;
+    private int report;
+    private int author;
+
+    public static class IngredientDto {
+        private String ingredient;
+        private String amount;
+
+
+    }
+
+    public static class ManualDto {
+        private String text;
+        private String imageUrl;
+
+
+    }
+}
+
