bbff074498bfbf4aa5e9e08727f1c178f87b3d85
kimju9908 <kimj9908@naver.com>
2025-03-06 11:45:46 +0900

    음식레시피 데이터

---
+++ back/src/main/java/com/kh/back/service/recipe/AddCocktailRecipeService.java
@@ -0,0 +1,63 @@
+package com.kh.back.service.recipe;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kh.back.dto.recipe.request.AddCocktailRecipeDto;
+import com.kh.back.service.FirebaseService;
+import com.kh.back.service.python.ElasticService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class AddCocktailRecipeService {
+    @Autowired
+    private ElasticService elasticService;
+    @Autowired
+    private FirebaseService firebaseService;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public String saveCocktailRecipe(Long memberId, AddCocktailRecipeDto recipeRequest) {
+        try {
+            String mainImageUrl = firebaseService.uploadImage(recipeRequest.getImage(),recipeRequest.getName());
+
+            List<Map<String, Object>> ingredients = recipeRequest.getIngredients().stream()
+                    .map(ingredient -> {
+                        Map<String, Object> ingredientMap = new HashMap<>();
+
+                        ingredientMap.put("unit", ingredient.getUnit());
+                        ingredientMap.put("amount", ingredient.getAmount());
+                        ingredientMap.put("ingredient", ingredient.getIngredient());
+                        ingredientMap.put("special", ingredient.getSpecial());
+                        return ingredientMap;
+                    })
+                    .collect(Collectors.toList());
+
+            // JSON 데이터 생성
+            Map<String, Object> recipeData = new HashMap<>();
+            recipeData.put("type", recipeRequest.getType());
+            recipeData.put("name", recipeRequest.getName());
+            recipeData.put("glass", recipeRequest.getGlass());
+            recipeData.put("category", recipeRequest.getCategory());
+            recipeData.put("ingredients", ingredients);
+            recipeData.put("garnish", recipeRequest.getGarnish());
+            recipeData.put("preparation", recipeRequest.getPreparation());
+            recipeData.put("abv", recipeRequest.getAbv());
+            recipeData.put("like", 0L); // 기본값 설정
+            recipeData.put("report", 0L); // 기본값 설정
+            recipeData.put("author", memberId);
+            recipeData.put("image", mainImageUrl);
+
+            // JSON 문자열로 변환 후 업로드
+            String data = objectMapper.writeValueAsString(recipeData);
+            return elasticService.uploadRecipe(data);
+        } catch (IOException e) {
+            return "레시피 저장 중 오류 발생: " + e.getMessage();
+        }
+    }
+}
